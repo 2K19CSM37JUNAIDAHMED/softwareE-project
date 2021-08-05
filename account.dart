b0d3ba5bad6ec8f688831e5dcf92fe327f29e515
junaid <junaidkhyber5@gmail.com>
2021-08-05 13:00:49 +0500

    menu of project defined

--- account.dart
+++ account.dart
@@ -11,3 +11,33 @@ String loginemail;
 String input;
 String oldpassword;
 String newpass;
+print("-----------------");
+
+print("ENTER ANY NUM :");
+
+print("-----------------");
+
+print("1. CREATE AN ACCOUNT");
+print("2. VIEW AN ACCOUNT");
+print("3. UPDATE AN ACCOUNT");
+print("4. DELETE AN ACCOUNT");
+
+String number = stdin.readLineSync();
+
+int num = int.parse(number);
+
+
+if (num == 1) //main if step;
+{
+
+  print("-----------------");
+
+  print("CREATE AN ACCOUNT");
+
+  print("-----------------");
+
+  print("ENTER NAME :");
+  name = stdin.readLineSync();
+
+  print("ENTER EMAIL : ");
+  email = stdin.readLineSync();
